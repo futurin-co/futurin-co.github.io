d7b3d7a716470ea9d5e8e4c7b55108554245af6e
Marcelo Almeida <2532492+marcelocra@users.noreply.github.com>
2024-10-31 14:28:06 -0300

    feat: create the main landing page

---
+++ src/lib/Card.svelte
@@ -0,0 +1,33 @@
+<script lang="ts">
+	let {
+		title,
+		content,
+		linkUrl,
+		linkText,
+		children
+	}: {
+		title?: string;
+		content?: string;
+		linkUrl?: string;
+		linkText?: string;
+		children?: any;
+	} = $props();
+</script>
+
+<!-- conditional -->
+
+<div
+	class="flex max-w-md flex-col gap-3 rounded-lg bg-white p-3 shadow-md transition-shadow duration-300 hover:shadow-lg"
+>
+	{#if children}
+		{@render children()}
+	{/if}
+
+	{#if !children}
+		<h2 class="mb-4 text-2xl font-semibold text-gray-800">{{ title }}</h2>
+		<p class="text-gray-600">{{ content }}</p>
+		<a href={linkUrl} class="mt-4 inline-block font-semibold text-blue-500 hover:text-blue-700"
+			>{{ linkText }}</a
+		>
+	{/if}
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import Card from '$lib/Card.svelte';
+</script>
+
+<div
+	class="mx-auto flex h-screen w-full flex-col items-center justify-center gap-3 overflow-hidden bg-slate-200"
+>
+	<Card>
+		<div class="flex flex-col items-center justify-center gap-3 p-3">
+			<span class="text-4xl font-light sm:text-5xl">Welcome to</span>
+			<span class="text-5xl font-bold sm:text-6xl">Futurin.co!</span>
+		</div>
+	</Card>
+</div>
